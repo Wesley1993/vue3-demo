a0ced04626b510082caef6e064a1482dcb72953d
wesley93 <1124700977@qq.com>
2020-12-15 23:25:58 +0800

    添加antd和axios依赖包

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
 import router from './router'
+import 'ant-design-vue/dist/antd.less'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store, Antd).use(router).mount('#app')
