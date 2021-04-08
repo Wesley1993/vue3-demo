457ed95b0bfdd164bf03762314bd97af224586f8
wesley93 <1124700977@qq.com>
2021-04-08 10:28:05 +0800

    ref()和setup()使用

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,41 @@
 <template>
-  <router-view/>
+  <!-- <router-view/> -->
+  <img src="./assets/logo.png" alt="" />
+  <div>
+    <h2>香飘飘水果店</h2>
+    <div>请选择需要的水果</div>
+  </div>
+  <div>
+    <button v-for="(item,index) in fruit" :key="index" @click="selectFruitFun(index)">
+      {{index}}:{{item}}
+    </button>
+  </div>
+  <div>
+    你买了【{{selectFruit}}】
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name:'App',
+  components:{},
+  setup() {
+    const fruit = ref(["葡萄","榴莲","菠萝蜜"])
+    const selectFruit=ref('')
+    const selectFruitFun = (index:number)=>{
+      selectFruit.value=fruit.value[index]
+    }
+    // 需要在模板中调用的方法和变量 都需要return出去
+    return {
+      fruit,
+      selectFruit,
+      selectFruitFun
+    }
+  },
+})
+</script>
 
 <style lang="less">
 #app {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Antd from 'ant-design-vue'
+import { createApp } from 'vue' // 引入vue文件，并导出 `createApp`
+import App from './App.vue' // 引入自定义组件，页面上看的东西基本都在这个组件里面
+import Antd from 'ant-design-vue' //
 import router from './router'
 import 'ant-design-vue/dist/antd.css'
 import store from './store'
@@ -8,4 +8,4 @@ import store from './store'
 const app = createApp(App)
 app.use(Antd)
 app.use(store)
-app.use(router).mount('#app')
+app.use(router).mount('#app') // 把APP组件挂载到#app节点上，在public目录下的index.html找节点
